9851ce9eaa57823f9c93fcefd61646c640ecc207
Ayatakanimeshi <ayatakathebattler2top@gmail.com>
2023-12-07 15:27:40 +0900

    学習1

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class TestScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    //Step1. 変数に入れるデータを指定して変数を作る
+    //Step2.データを変数に割り当て
+    //Step.3変数の中のデータを使用する
+
     void Start()
     {
-        
-    }
+        int number;
 
-    // Update is called once per frame
-    void Update()
-    {
+        number = 3;
+
+        Debug.Log(number);
         
     }
 }
