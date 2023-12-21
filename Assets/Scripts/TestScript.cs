7f69b8ab0b561bd626339678f26ece30a401a895
Ayatakanimeshi <ayatakathebattler2top@gmail.com>
2023-12-21 16:44:34 +0900

    study23

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -9,43 +9,15 @@ public class TestScript : MonoBehaviour
 
     void Start()
     {
-        int scoreA = 100;
-        int scoreB = 150;
-        int scoreC = 170;
-        int scoreD = 200;
+        float targetNumber = 11.5f;
 
-        Debug.Log(SumScore(scoreA, scoreB));
-        Debug.Log(SumScore(scoreA, scoreB, scoreC));
-        Debug.Log(SumScore(scoreA, scoreB, scoreC, scoreD));
-    }
+        float floorResult = Mathf.Floor(targetNumber);
+        Debug.Log(floorResult);
 
-    //???????????????
-    int SumScore(int score1, int score2)
-    {
-        int totalScore;
+        float ceilResult = Mathf.Ceil(targetNumber);
+        Debug.Log(ceilResult);
 
-        totalScore = score1 + score2;
-
-        return totalScore;
-    }
-
-    //???????????????
-    int SumScore(int score1, int score2, int score3)
-    {
-        int totalScore;
-
-        totalScore = score1 + score2 + score3;
-
-        return totalScore;
-    }
-
-    //???????????????
-    int SumScore(int score1, int score2, int score3, int score4)
-    {
-        int totalScore;
-
-        totalScore = score1 + score2 + score3 + score4;
-
-        return totalScore;
+        float roundResult = Mathf.Round(targetNumber);
+        Debug.Log(roundResult);
     }
 }
